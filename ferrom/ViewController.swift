e38c2f71937bbb155af9bb2c6557288474152710
ciecS20171106331 <565738655@qq.com>
2018-10-13 10:06:39 +0800

    caculator
    jiafa

--- ferrom/ViewController.swift
+++ ferrom/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBAction func jiafa(_ sender: Any) {
+        var cx=0
+        cx=Int(x.text!)!
+        var cy=0
+        cy=Int(y.text!)!
+        var cz:Int=0
+        cz=cx+cy
+        z.text="\(cz)"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
